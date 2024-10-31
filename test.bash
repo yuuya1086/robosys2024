879c6185f961edbd9c5cab2fd420961402231970
Yuuya Tanaka <s23c1086uu@s.chibakoudai.jp>
2024-10-31 12:31:21 +0900

    Change name

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
 	echo ${1}行目が違うよ
@@ -7,7 +7,7 @@ ng () {
 
 res=0
 
-out=$(seq 2 | ./plus)
+out=$(seq 5 | ./plus)
 [ "$out" = 15 ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
