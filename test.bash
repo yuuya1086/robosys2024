6ebaeec2b42acc10760e53d0f7f33de2e62f20c8
Yuuya Tanaka <s23c1086uu@s.chibakoudai.jp>
2024-10-31 12:41:01 +0900

    CHANGE NAME

--- test.bash
+++ test.bash
@@ -10,6 +10,14 @@ res=0
 out=$(seq 5 | ./plus)
 [ "$out" = 15 ] || ng "$LINENO"
 
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]    || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]    || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
 [ "$res" = 0 ] && echo OK
 
 exit "$res"
