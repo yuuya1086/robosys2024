813e70de6f9becf70ad580183caece5d659efd7a
Yuuya Tanaka <s23c1086uu@s.chibakoudai.jp>
2024-11-27 17:01:09 +0900

    Add a command

---
+++ BMI.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2024 Yuuya Tanaka s23c1086uu@s.chibakoudai.jp
+# SPDX-License-Identifier: BSD-3-Clause
+
+import sys
+
+n = float(sys.argv[1])
+m = float(sys.argv[2])
+
+n = n / 100
+
+BMI = m / (n * n)
+tekisei = (n * n) * 22
+
+print("あなたのBMIは"'{:.2f}'.format(BMI))
+print("適正体重は"'{:.1f}'.format(tekisei))
+
+if BMI < 18.50:
+
+    print("瘦せ型です")
+
+elif BMI > 25.00:
+
+    print("肥満です")
+
+else:
+
+    print("適正です")
--- test.bash
+++ test.bash
@@ -7,16 +7,19 @@ ng () {
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "$out" = 15 ] || ng "$LINENO"
+out=$(./BMI.py 170 60)
+expected="あなたのBMIは20.76
+適正体重は63.6
+適正です"
+[ "${out}" = "${expected}" ] || ng "$LINENO"
 
-out=$(echo あ| ./plus)
-[ "$?" = 1 ]    || ng "$LINENO"
-[ "$out" = "" ] || ng "$LINENO"
+out=$(./BMI.py 170)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]    || ng "$LINENO"
-[ "$out" = "" ] || ng "$LINENO"
+out=$(./BMI.py)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 
