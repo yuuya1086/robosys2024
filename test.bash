1d4a5765daf870cb4b576208c5721bd74e674e56
Yuuya Tanaka <s23c1086uu@s.chibakoudai.jp>
2024-11-27 18:05:44 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -8,12 +8,36 @@ ng () {
 res=0
 
 out=$(./BMI.py 170 60)
-expected="あなたのBMIは20.76
-適正体重は63.6
+expected="あなたのBMI値は20.76
+適正体重は63.6kg
 適正です"
 [ "${out}" = "${expected}" ] || ng "$LINENO"
 
+out=$(./BMI.py a a)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(./BMI.py あ あ)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(./BMI.py a あ)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(./BMI.py あ a)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 out=$(./BMI.py 170)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(./BMI.py a)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(./BMI.py あ)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
